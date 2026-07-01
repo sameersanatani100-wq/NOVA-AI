e90c6d26c8d80af5d2d50f79d5b228c12bed32d0
sameersanatani100-wq <sameersanatani100@gmail.com>
2026-07-02 00:34:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -107,14 +107,18 @@ if (clearBtn) {
     };
 }
 const menuBtn = document.getElementById("menuBtn");
+const menuBtn = document.getElementById("menuBtn");
 const sidebar = document.getElementById("sidebar");
+const overlay = document.getElementById("overlay");
 
-console.log(menuBtn);
-console.log(sidebar);
-
-if (menuBtn && sidebar) {
+if (menuBtn && sidebar && overlay) {
     menuBtn.addEventListener("click", () => {
         sidebar.classList.toggle("active");
-        console.log("Menu clicked");
+        overlay.classList.toggle("show");
+    });
+
+    overlay.addEventListener("click", () => {
+        sidebar.classList.remove("active");
+        overlay.classList.remove("show");
     });
 }
